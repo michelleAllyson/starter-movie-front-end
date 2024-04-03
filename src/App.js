03fe5a15a085406a78c871d6cde6f841378f11da
Michelle Morrow <michelle.allyson.morrow@gmail.com>
2024-04-03 16:00:18 -0400

    Added backend deployed URL

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import DetailedMoviesList from "./movies/DetailedMoviesList";
 import FullMovie from "./movie/FullMovie";
 import TheaterList from "./theaters/TheaterList";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { API_BASE_URL } from "./utils/api";
 
 function App() {
   return (
